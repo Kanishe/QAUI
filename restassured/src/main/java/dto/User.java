32222f2866a56b2d704aa1ea77250734d2248f41
Alexander Kanishevskii <a.kan4988@gmail.com>
2020-11-06 11:27:25 +0300

    fix Pom

---
+++ restassured/src/main/java/dto/User.java
@@ -0,0 +1,80 @@
+
+package dto;
+
+public class User {
+    private String email;
+    private String firstName;
+    private Long id;
+    private String lastName;
+    private String password;
+    private String phone;
+    private Long userStatus;
+    private String username;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Long getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(Long userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
+}
