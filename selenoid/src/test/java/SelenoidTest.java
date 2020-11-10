95bbdff68d15728536581b99f280d706f50db7fc
Alexander Kanishevskii <a.kan4988@gmail.com>
2020-11-10 22:10:16 +0300

    create initial test for selenoid

---
+++ selenoid/src/test/java/SelenoidTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SelenoidTest {
+    public static WebDriver driver;
+    @BeforeEach
+    public static void setUp () throws MalformedURLException  {
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setBrowserName("chrome");
+            desiredCapabilities.setVersion("78.0");
+            desiredCapabilities.setCapability("enableVNC",true);
+            desiredCapabilities.setCapability("enableVideo",false);
+            driver = new RemoteWebDriver(
+                    new URL("http://0.0.0.0:4444/wd/hub"),desiredCapabilities
+            );
+
+
+    }
+}
